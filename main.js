3759503c249326c8c0bb61665c3fb89760b76abb
Ernesto-B <ernesto.barreto@ucalgary.ca>
2023-01-24 02:15:28 -0700

    commented code for future revision

--- main.js
+++ main.js
@@ -1,33 +1,47 @@
+// Getting the form form the DOM
 const form = document.querySelector('form');
 
+// Listen to when user clicks button and submits the form
 form.addEventListener('submit', async (e) => {
+  
+  // Prevents page from refreshing when submitting the form
   e.preventDefault();
     showSpinner();
-    const data = new FormData(form);
+  
+  // Extract data from form by using FormData object that takes form as the input. Behaves like a map
+  const data = new FormData(form);
 
-    const response = await fetch('http://localhost:5173/dream', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        prompt: data.get('prompt'),
-      }),
-    });
+  // Make request to API
+  const response = await fetch('http://localhost:5173/dream', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    // Convert recieved object into string
+    body: JSON.stringify({
+      // Make object prompt which grabs prompt value from the form using get method
+      prompt: data.get('prompt'),
+    }),
+  });
     
 
-    if (response.ok) {
-      const { image } = await response.json();
+  if (response.ok) {
+    // Save the url of the image if API connection and response was successful 
+    const { image } = await response.json();
 
-      const result = document.querySelector('#result');
-      result.innerHTML = `<img src="${image}" width="512" />`;
-    } else {
-      const err = await response.text();
-      alert(err);
-      console.error(err);
-    }
+    // Insert image into the UI
+    const result = document.querySelector('#result');
+    result.innerHTML = `<img src="${image}" width="512" />`;
+  } 
+  
+  else {
+    // Alert the user that there was an issue with the request, prompt, or connection
+    const err = await response.text();
+    alert(err);
+    console.error(err);
+  }
 
-    hideSpinner();
+  hideSpinner();
 
 });
 
